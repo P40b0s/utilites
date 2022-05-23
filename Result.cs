3e2d0f80482865fdde741e583773511288712127
p40b0s <uranius666@gmail.com>
2022-05-23 15:28:41 +0300

    Добавил Result<T,E>

--- Result.cs
+++ Result.cs
@@ -3,15 +3,15 @@ using System.Runtime.CompilerServices;
 
 namespace Utils;
 
-public interface IResult<T>
+public interface IResult<T, E>
 {
     T Value([CallerMemberName]string caller = null);
-    IError Error([CallerMemberName]string caller = null);
+    E Error([CallerMemberName]string caller = null);
     bool IsOk {get;}
     bool IsError {get;}
 }
 
-public struct Result<T> : IResult<T>
+public struct Result<T> : IResult<T, IError>
 {
     public Result(T result)
     {
@@ -59,6 +59,38 @@ public struct Result<T> : IResult<T>
     public static Result<T> Ok(T value)
     {
         return new Result<T>(value);
+    }
+}
+public struct Result<T,E> : IResult<T, E>
+{
+    public Result(E error)
+    {
+        this.error = error;
+        value = default(T);
+    }
+    /// <summary>
+    /// Значение результата
+    /// </summary>
+    /// <returns></returns>
+    public T Value([CallerMemberName]string caller = null)
+    {
+        if(this.IsError)
+            throw new Exception($"Метод {caller} пытается получить результат операции, со статусом ERROR");
+        else return value;
+    }
+    private T value {get;init;}
+    public E Error([CallerMemberName]string caller = null)
+    {
+        if(!this.IsError)
+            throw new Exception($"Метод {caller} пытается получить ошибку операции, но операция завершена положительно и у нее есть результат: " + Value().ToString());
+        else return error;
+    }
+    private E error {get;init;}
+    public bool IsOk => error == null;
+    public bool IsError => error != null;
+    public static Result<T, E> Err(E error)
+    {
+        return new Result<T, E>(error);
     }
 }
 
